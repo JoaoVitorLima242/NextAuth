0a7ded55211ce3ba543b9703145907271870a126
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-08 20:06:42 -0300

    Set user by token

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -1,7 +1,8 @@
-import { createContext, useState } from "react";
-import { signInRequest } from "../services/auth";
-import { setCookie } from "nookies"
+import { createContext, useEffect, useState } from "react";
 import Router from 'next/router'
+import { setCookie, parseCookies } from "nookies"
+
+import { recoverUserInformation, signInRequest } from "../services/auth";
 interface AuthCtx {
     isAuthenticated: boolean;
     user: User
@@ -26,6 +27,14 @@ export function AuthProvider ({ children }) {
     const [user, setUser] = useState<User | null>(null)
 
     const isAuthenticated = !!user
+
+    useEffect(() => {
+        const { 'nextauth-token': token } = parseCookies()
+
+        if(token) {
+            recoverUserInformation().then(response => setUser(response.user))
+        }
+    }, [])
     
     async function signIn({email, password}: SignInData) {
         const { token, user } = await signInRequest({ email, password })
