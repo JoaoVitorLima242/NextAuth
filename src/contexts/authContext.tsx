24b060636d8a3673ab98627f89d92fad9b583b1e
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-08 19:24:02 -0300

    create signin and save data

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -1,16 +1,47 @@
-import { createContext } from "react";
-
-export const AuthContext = createContext({})
-
-interface authCtx {
-    isAuthenticated: boolean
+import { createContext, useState } from "react";
+import { signInRequest } from "../services/auth";
+import { setCookie } from "nookies"
+import Router from 'next/router'
+interface AuthCtx {
+    isAuthenticated: boolean;
+    user: User
+    signIn: (data: SignInData) => Promise<void>
 }
 
+type SignInData = {
+    email: string;
+    password: string
+}
+
+type User = {
+    name: string
+    email: string
+    avatar_url: string
+}
+
+export const AuthContext = createContext({} as AuthCtx)
+
+
 export function AuthProvider ({ children }) {
-    const isAuthenticated = false
+    const [user, setUser] = useState<User | null>(null)
+
+    const isAuthenticated = !!user
+    
+    async function signIn({email, password}: SignInData) {
+        const { token, user } = await signInRequest({ email, password })
+
+        setCookie(undefined, 'nextauth-token', token, {
+            maxAge: 60 * 60 * 1 // 1 hour 
+        })
+
+        setUser(user)
+
+        Router.push('/dashboard')
+        
+    }
 
     return (
-        <AuthContext.Provider value={{isAuthenticated}}>
+        <AuthContext.Provider value={{isAuthenticated, signIn, user}}>
             {children}
         </AuthContext.Provider>
     )
---
+++ src/services/auth.ts
@@ -0,0 +1,21 @@
+import {v4 as uuid} from 'uuid'
+
+type SigInRequestData = {
+    email: string;
+    password: string;
+}
+
+const delay = (amount = 750) => new Promise (resolve => setTimeout(resolve, amount))
+
+export async function signInRequest(data: SigInRequestData) {
+    await delay()
+
+    return {
+        token: uuid(),
+        user: {
+            name: "Joaozin",
+            email: "joaozin@email.com",
+            avatar_url: 'https://github.com/JoaoVitorLima242.png'
+        }
+    }
+}
